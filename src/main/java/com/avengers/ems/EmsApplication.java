8f9c43659898a3581cd0710084493cce02309d80
US\ssaisravanth <ssaisravanth@deloitte.com>
2022-09-07 22:40:37 +0530

    DE-3 commit twice

--- src/main/java/com/avengers/ems/EmsApplication.java
+++ src/main/java/com/avengers/ems/EmsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmsApplication {
-	//dummy commit in main
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EmsApplication.class, args);
